170a1a22b83e495f05adce955633c1b00032ef52
LucinaBueno <lucina.bueno@jwmsolutions.com>
2024-06-28 09:59:58 -0600

    Cart
    - Change modal style.
    - Add a dropdown to select the product's quantity.
    - Update the cart adding the quantity selected.
    - On the cart icon show the total amount of products selected.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -56,7 +56,6 @@ export default function Header(props) {
         props.setBrandColor(data.primary_color);
       })
       .then(() => setIsLoaded(true));
-
   }, []);
 
   // On changing the currency, empty the cart
@@ -91,7 +90,7 @@ export default function Header(props) {
                 style={{ textDecoration: "none", color: "inherit" }}
               >
                 <FontAwesomeIcon icon={faShoppingCart} />{" "}
-                {props.cart?.lineItems.length ? props.cart.lineItems.length : 0}
+                {props.totalQuantity}
               </Link>
             </div>
           )}
