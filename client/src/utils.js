63e7ab88cbc86d87dd70ce4d20554c4ca004300c
Aaron Meza <aaron.meza@gluo.mx>
2025-06-03 11:44:37 -0600

    Add server-side API for Stripe and CommerceTools integration

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,5 +1,5 @@
 const projectKey = process.env.REACT_APP_PROJECT_KEY;
-const BACKEND_URL = process.env.REACT_APP_BASE_URL;
+const BACKEND_URL = process.env.NODE_ENV === 'production' ? '' : process.env.REACT_APP_BASE_URL;
 
 
 export const loadEnabler = async() => {
