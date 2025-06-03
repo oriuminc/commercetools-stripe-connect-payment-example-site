ed05e5f4f3cf5bdcd9990f20c4e790d6df6e6230
Aaron Meza <aaron.meza@gluo.mx>
2025-06-03 17:23:26 -0600

    Update backend URL configuration in Header.js: use CURRENT_ENV for production check and replace VERCEL_URL with NEXT_PUBLIC_VERCEL_URL for improved clarity

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,8 @@ import { DEV_REQUEST_HEADERS } from "../utils";
 import logo from "../images/logo.svg";
 import SwitchSelector from "react-switch-selector";
 
-const BACKEND_URL = process.env.VERCEL_URL || "http://localhost:3000";
+
+const BACKEND_URL = process.env.CURRENT_ENB === 'production' ? process.env.NEXT_PUBLIC_VERCEL_URL : "http://localhost:3000";
 
 export default function Header(props) {
   const [isLoaded, setIsLoaded] = useState(false);
